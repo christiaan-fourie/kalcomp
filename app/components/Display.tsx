68c8c68b2afb5505d8758829379fac425f7af2cc
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-06-13 11:10:19 +0200

    Create Display.tsx

---
+++ app/components/Display.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+const Display = () => {
+  return (
+    <div className='w-full text-end text-7xl font-mono p-12'>
+      0
+    </div>
+  )
+}
+
+export default Display
