898e1567202d349febeb6c85d191b794349dfb63
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-06-15 08:45:39 +0200

    Refactor the calculation utility

--- app/utils/calculator.ts
+++ app/utils/calculator.ts
@@ -1,13 +1,14 @@
 // Calculate a string without using eval
 export function calculate(str: string): number {
-    str = str.replace(/\s+/g, ''); // Remove spaces from the input string
-    // Replace instances of 'x' and '÷' with '*' and '/'
-    str = str.replace(/x/g, '*').replace(/÷/g, '/');
-    
+
+    // Preprocess the input string    
+    str = str.replace(/\s+/g, ''); // Remove all spaces from the input string    
+    str = str.replace(/x/g, '*').replace(/÷/g, '/'); // Replace instances of 'x' and '÷' with '*' and '/'
+    str = str.replace(/--/g, '+'); // Replace all instances of '--' with '+'
   
     // Check for invalid characters
     if (!/^[\d+\-*/\(\)\.]+$/.test(str)) {
-      throw new Error('Invalid characters in the input string.');
+      str='Invalid characters in the input string.';
     }
   
     // Handle parentheses
@@ -15,18 +16,19 @@ export function calculate(str: string): number {
       const start = str.lastIndexOf('(');
       const end = str.indexOf(')', start);
       if (start === -1 || end === -1) {
-        throw new Error('Mismatched parentheses in the input string.');
+        str='Mismatched parentheses in the input string.';
       }
       const result = calculate(str.slice(start + 1, end));
       str = str.slice(0, start) + result + str.slice(end + 1);
     }
   
-    const performOperation = (operator, operation) => {
+    // Handle negative values
+    const performOperation = (operator: string, operation: any ) => {
       while (str.includes(operator)) {
         const regex = new RegExp(`(-?\\d+(?:\\.\\d+)?)(?:\\${operator})(-?\\d+(?:\\.\\d+)?)`);
         const match = str.match(regex);
         if (!match) {
-          throw new Error('Invalid input string.');
+          str='Invalid input string.';
         }
         const leftValue = parseFloat(match[1]);
         const rightValue = parseFloat(match[2]);
@@ -34,16 +36,20 @@ export function calculate(str: string): number {
         str = str.replace(regex, result);
       }
     };
-  
-    performOperation('*', (a, b) => a * b);
-    performOperation('/', (a, b) => {
+
+    // Handle multiplication and division
+    performOperation('*', (a: number, b:number) => a * b);
+    performOperation('/', (a:number, b:number) => {
       if (b === 0) {
-        throw new Error('Division by zero is not allowed.');
+        str='Division by zero is not allowed.';
       }
       return a / b;
     });
-    performOperation('+', (a, b) => a + b);
-    performOperation('-', (a, b) => a - b);
+
+    // Handle addition and subtraction
+    performOperation('+', (a: number, b:number) => a + b);
+    performOperation('-', (a: number, b:number) => a - b);
   
+    // Return the final result
     return parseFloat(str);
 }
